00151b149453ebdbb4cd2d8cd8c0aa0c2944ac9b
Álvaro Cañas <acaygo@gmail.com>
2024-10-08 00:27:30 +0200

    css

---
+++ practicas_examen/ud1_ud2/js/index.js
@@ -0,0 +1,9 @@
+// Variables.
+let frase = "";
+
+frase = prompt("Introduzca una frase: ");
+if(charCodeAt(32) > 1) {
+    alert("El texto introducido tiene más de un espacio.");
+} else {
+    alert(`La frase introducida es la siguiente: ${frase}`);
+}
