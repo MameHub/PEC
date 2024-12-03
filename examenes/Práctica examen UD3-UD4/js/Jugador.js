ad77a4aac18b45523b383a5ab196fcce2213359a
Álvaro Cañas <acaygo@gmail.com>
2024-12-03 13:16:15 +0100

    creación de clases y form

---
+++ examenes/Práctica examen UD3-UD4/js/Jugador.js
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+class Jugador {
+
+    #nombre;
+    #edad;
+
+    constructor(nombre, edad) {
+        this.#nombre = nombre;
+        this.#edad = edad;
+    }
+
+    toString() {
+        return `El jugador se llama ${this.#nombre} y tiene ${this.#edad} años.`;
+    }
+
+    get nombre() {
+        return this.#nombre;
+    }
+
+    get edad() {
+        return this.#edad;
+    }
+
+    set nombre(nombre) {
+        if (String(nombre.lenght()) <= 3 && String(nombre.lenght()) >= 50) {
+            this.#nombre = nombre;            
+        } else
+            return "El nombre introducido del jugador debe de tener una longitud entre 3 y 50 caracteres.";
+    }
+
+    set edad(edad) {
+        if (edad > 10 && edad < 60) {
+            this.#edad = edad;
+        } else
+            return "El jugador debe de tener una edad de entre 10 y 60 años.";
+    }
+
+}
---
+++ examenes/Práctica examen UD3-UD4/js/Partido.js
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+class Partido {
+
+    #jugadores;
+    #Sets;
+
+    constructor(jugadores, sets) {
+        this.#jugadores = jugadores;
+        this.#Sets = sets;
+    }
+
+    toStrig() {
+        return ``;
+    }
+
+    get jugadores() {
+        return this.#jugadores;
+    }
+
+    get sets() {
+        return this.#Sets;
+    }
+
+    set jugador(jugador) {
+        if (condition) {
+            
+        }
+    }
+
+    set sets(sets) {
+        if (condition) {
+            
+        }
+    }
+
+}
