19379db1e6991f66d26d84cf33108b6ec5fbdd41
Álvaro Cañas <acaygo@gmail.com>
2025-01-16 13:07:19 +0100

    ah

---
+++ UD2-CLASE/Actividades/ej4/js/index.js
@@ -0,0 +1,9 @@
+// function randomNumber(){
+    let A = Math.random;
+    let B = Math.random;
+    while(A===B)
+        A = Math.random;
+    console.log(`A = ${A}, B = ${B}`);
+// }
+
+// randomNumber();
