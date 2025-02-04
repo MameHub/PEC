1fe674386260d0e87180ba49455e355bcc350858
MameHub <acaygo@gmail.com>
2025-02-04 12:29:40 +0100

    ah

---
+++ UD5-CLASE/Ejercicios/juegoParejas/js/app.js
@@ -0,0 +1,38 @@
+//Verificamos si las dos caras seleccionadas son la misma
+const verificarPareja = () => {
+    let valor1 = primeraCarta.getAttribute("data-valor");
+    let valor2 = segundaCarta.getAttribute("data-valor");
+
+    if(valor1 == valor2){
+    //Son pareja
+        //Elimino el evento click de esas dos cartas
+        primeraCarta.removeEventListener("click",voltearCarta);
+        segundaCarta.removeEventListener("click",voltearCarta);
+        //Aumento el contador de parejas encontradas 
+        parejasEncontradas ++;
+        //Comprobamos si ya ha encontrado todas las parejas
+        if(parejasEncontradas == 6) {
+            mostrarMensaje("Felicidades!!","success");
+        }
+    else{
+        //No son pareja
+        //Pasando un segundo limpio los DIVs
+        setTimeout(() => {
+            primeraCarta.classList.remove("volteada");
+            segundaCarta.classList.remove("volteada");
+            primeraCarta.textContent = "";
+            segundaCarta.textContent = "";
+            primeraCarta = null;
+            segundaCarta = null;
+        }, 1000);
+    }
+    }
+}
+
+//Función para mostrar un mensaje emergende usando SweetAlert2
+const mostrarMensaje = (texto, icono) => {
+    Swal.file({
+        position: "center",
+        
+    })
+}
