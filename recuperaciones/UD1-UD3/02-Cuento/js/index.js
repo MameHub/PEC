2ab4259ac82f7ab995d8a59d8809f037eae43bbb
Álvaro Cañas <acaygo@gmail.com>
2025-03-31 03:11:40 +0200

    ah

--- recuperaciones/UD1-UD3/02-Cuento/js/index.js
+++ recuperaciones/UD1-UD3/02-Cuento/js/index.js
@@ -14,10 +14,13 @@ try {
     const personaje4 = new Personaje("Paulinha", "Yuko", 1);
 
     // 3 Protagonistas
-
+    const protagonista1 = new Protagonista("Mamev0", "Clavate", 27, "furia");
+    const protagonista2 = new Protagonista("Yisus", "Lilty", 29, "frenesi");
+    const protagonista3 = new Protagonista("Naza", "Selki", 16, "alto rendimiento");
 
     // 2 Escenarios
-
+    const escenario1 = new Escenario("", "", "");
+    const escenario2 = new Escenario("", "", "");
 
     // Cuento
     const cuento1 = new Cuento();
