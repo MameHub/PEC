8d714e6c96b5441e2da1fc3baeb2f86f75d1b771
Álvaro Cañas <acaygo@gmail.com>
2025-03-31 03:02:53 +0200

    clase escenario completa

--- recuperaciones/UD1-UD3/02-Cuento/js/cuento.js
+++ recuperaciones/UD1-UD3/02-Cuento/js/cuento.js
@@ -1,3 +1,6 @@
+// Importamos el módulo protagonista para el método "anadirPersonaje()".
+import { Protagonista } from "./protagonista";
+
 // Creamos la clase "Cuento" de forma que se pueda exportar para utilizar en otro archivo o puedan heredarla.
 export class Cuento {
 
@@ -15,8 +18,8 @@ export class Cuento {
         this.autor = autor;
         this.moraleja = moraleja;
         this.descripcion = descripcion;
-        this.#aEscenarios = [];
-        this.#aPersonajes = [[],[]];
+        this.#aEscenarios = [];             // Array de escenarios.
+        this.#aPersonajes = [[],[]];        // Array bidimensional de personajes.
     }
 
     // Getter and Setters
@@ -25,7 +28,11 @@ export class Cuento {
     }
 
     set titulo(titulo) {
-        
+        if (titulo.length < 3) {
+            throw new Error("El título debe de tener 3 caracteres mínimo.");
+        } else {
+            this.#titulo = titulo;
+        }
     }
 
     get autor() {
@@ -33,7 +40,11 @@ export class Cuento {
     }
 
     set autor(autor) {
-
+        if (condition) {
+            throw new Error("Debe de introducir un nombre para el autor.");
+        } else {
+            this.#autor = autor;
+        }
     }
 
     get moraleja() {
@@ -41,7 +52,11 @@ export class Cuento {
     }
 
     set moraleja(moraleja) {
-
+        if (moraleja.length > 100) {
+            throw new Error("La moraleja del cuento no puede tener más de 100 caracteres.");
+        } else {
+            this.#moraleja = moraleja;
+        }
     }
 
     get descripcion() {
@@ -49,19 +64,38 @@ export class Cuento {
     }
 
     set descripcion(descripcion) {
-
+        if (descripcion.length > 300) {
+            throw new Error("La descripción del cuento no puede exceder los 300 caracteres.");
+        } else {
+            this.#descripcion = descripcion;
+        }
     }
 
     introducirEscenario(escenario) {
-        
+        for (const e of this.#aEscenarios) {
+            if (e.nombre === escenario.nombre) {
+                return false;
+            }
+        }
+        return true;
     }
 
     eliminarEscenario(escenario) {
-
+        for (let i = 0; i < this.#aEscenarios.length; i++) {
+            if (this.#aEscenarios[i].nombre === escenario.nombre) {
+                this.#aEscenarios.splice(i,1);
+                return true;
+            }
+        }
+        return true;
     }
 
-    annadirPersonaje() {
-        
+    annadirPersonaje(personaje) {
+        if (personaje instanceof Protagonista) {
+            this.#aPersonajes[1].push(personaje);
+        } else {
+            this.#aPersonajes[0].push(personaje);
+        }
     }
 
     // Obtenemos una cadena de texto con la información del objeto creado.
