f398d3d737502445c70900c3c161d8ae454c1c82
Álvaro Cañas <acaygo@gmail.com>
2025-04-30 03:20:20 +0200

    ah

---
+++ recuperaciones/UD4/practicas/actividad_1/js/index.js
@@ -0,0 +1,30 @@
+
+
+function validacionNombre() {
+    const nombreCompleto = document.getElementById("nombreCompleto");
+    console.log(nombreCompleto);
+}
+
+function validacionNombre() {
+
+}
+
+function validacionNombre() {
+
+}
+
+function validacionNombre() {
+
+}
+
+function validacionNombre() {
+
+}
+
+function validacionNombre() {
+
+}
+
+function validacionNombre() {
+
+}
